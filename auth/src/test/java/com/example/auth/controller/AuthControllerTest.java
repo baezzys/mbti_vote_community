bb103f5eaa090289e3ec3c82745184028f93199d
jinwoo-Bae <jinwoobae@sk.com>
2023-07-31 13:04:44 +0900

    test container 추가

---
+++ auth/src/test/java/com/example/auth/controller/AuthControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.auth.controller;
+
+import com.example.auth.payload.SignUpRequest;
+import com.example.auth.testcontainer.MbtiTestContainers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@MbtiTestContainers
+@AutoConfigureMockMvc
+@SpringBootTest
+public class AuthControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void signUpTest() throws Exception {
+
+        // given
+        SignUpRequest signUpRequest = new SignUpRequest();
+
+        // when
+        mockMvc.perform(post("/auth/signup", signUpRequest));
+
+        // then
+    }
+}
---
+++ auth/src/test/java/com/example/auth/testcontainer/MbtiTestContainers.java
@@ -0,0 +1,41 @@
+package com.example.auth.testcontainer;
+
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@org.testcontainers.junit.jupiter.Testcontainers
+@ContextConfiguration(initializers = {MbtiTestContainers.Initializer.class})
+@TestPropertySource(properties = {"spring.config.location=classpath:application-test.yml"})
+@ActiveProfiles("test-container")
+public @interface MbtiTestContainers {
+
+    @Container
+    MySQLContainer mySQLContainer = new MySQLContainer<>("mysql:8.0.30")
+            .withDatabaseName("kakao_gift")
+            .withInitScript("data/schema.sql")
+            .withCommand("mysqld --max_connections=200");
+
+    class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            mySQLContainer.start();
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + mySQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + mySQLContainer.getUsername(),
+                    "spring.datasource.password=" + mySQLContainer.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+}
